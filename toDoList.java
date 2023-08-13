26811b5a3314e25eb26570b1b96c0900cf70a156
Marie Chalard <mariechalard@MacBook-Air-de-Marie.local>
2023-08-13 18:32:20 +0200

    starting elements

--- toDoList.java
+++ toDoList.java
@@ -6,6 +6,8 @@ public class toDoList {
 
     String name;    // name of list
     Date date;      // date
+    element elements[];
+    int numberOfElement;
 
     // constructor
     public toDoList(String newList) {
@@ -22,4 +24,15 @@ public class toDoList {
         this.date = other.date;
     }
 
+    /** @summary add a new element to the list */
+    public void addElement(element newElement) {
+        element newElements[] = new element[numberOfElement];
+        for (int i = 0;  i < numberOfElement - 1; i++) { // number of list -1
+            newElements[i] = new element(null);
+            newElements[i].copyValue(elements[i]);
+        }
+        newElements[numberOfElement - 1] = new element(null);
+        newElements[numberOfElement - 1].copyValue(newElement);
+        elements = newElements;
+    }
 }
